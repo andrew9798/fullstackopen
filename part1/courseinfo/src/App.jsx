f89802bbc7ad9a57af0a2e6d1f1aa34e6f1896c3
andgan <andresganga1298@gmail.com>
2025-09-23 19:46:42 +0200

    Ej 1.3 y 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,13 +6,28 @@ import Total from "./Total"
 
 const App = () => {
 
+    const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   
 
   return (
     <>
-      <Header course="Half Stack application development" />
-      <Content />
-      <Total exercises1={10} exercises2={7} exercises3={14} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
     </>
   )
 }
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,18 +1,12 @@
 import Part from "./part"
 
-const Content = ( ) => {
+const Content = ({ parts }) => {
 
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
     return (
         <>
-            <Part part={part1} exercises={exercises1} />
-            <Part part={part2} exercises={exercises2} />
-            <Part part={part3} exercises={exercises3} />
+            {parts.map(part => (
+                <Part  part={part.name} exercises={part.exercises} />
+            ))}
         </>
     )
 }
